ac73f9f49b1729762538436492236a8496607895
Brian Lee <brianalee@gmail.com>
2023-07-31 15:26:12 -0400

    Update fetch_data_json.sh
    CDC stopped putting out code.json and it was causing script to barf. Commented that out since there's no real need to track versions of it any longer/

--- fetch_data_json.sh
+++ fetch_data_json.sh
@@ -5,7 +5,8 @@ curl -Ls https://data.cdc.gov/data.json > data/cdc.gov.data.json
 python -m json.tool data/cdc.gov.data.json > data/pretty.cdc.gov.data.json
 
 # I suppose I want code.json files to see how those change over time with hhs and cdc
+# CDC stopped publishing this around July, 2023
 curl -Ls https://www.hhs.gov/code.json > data/hhs.gov.code.json
 python -m json.tool data/hhs.gov.code.json > data/pretty.hhs.gov.code.json
-curl -Ls https://www.cdc.gov/code.json > data/cdc.gov.code.json
-python -m json.tool data/cdc.gov.code.json > data/pretty.cdc.gov.code.json
+#curl -Ls https://www.cdc.gov/code.json > data/cdc.gov.code.json
+#python -m json.tool data/cdc.gov.code.json > data/pretty.cdc.gov.code.json
